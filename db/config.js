d8a1433cc87a028de071dba537ae942dd46bddfc
Jorge Veron <jorveron.93@gmail.com>
2023-12-03 12:00:52 -0300

    configuración para la conexión a bd

--- db/config.js
+++ db/config.js
@@ -1,16 +1,19 @@
 const { config } = require('../config/config');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 module.exports = {
   development: {
-    url: URI,
-    dialect: 'postgres'
+    url: config.dbUrl,
+    dialect: 'postgres',
+    logging: (msg, time, level) => {
+      console.log(`${level} ${msg} (${time} ms)`)
+    }
   },
   production: {
-    url: URI,
-    dialect: 'postgres'
+    url: config.dbUrl,
+    dialect: 'postgres',
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 }
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -4,17 +4,23 @@ const { config } = require('../config/config');
 const setupModels = require('./../db/models');
 
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const options = {
+  dialect: 'postgres',
+  logging: config.isProd ? false : (msg, time, level) => {
+    console.log(`${level} ${msg} (${time} ms)`)
+  }
+}
 
+if (config.isProd) {
+  options.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+}
 
-const sequelize = new Sequelize(URI, {
-  dialect: 'postgres'
-});
+const sequelize = new Sequelize(config.dbUrl, options);
 
 setupModels(sequelize);
-
-
 
 module.exports = sequelize;
